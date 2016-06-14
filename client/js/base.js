31f55b560b6a39c314ea9eefdb77b5d17a32984f
Jessamyn Smith <geekchick77@gmail.com>
2016-06-13 20:55:28 -0700

    Added simple get and post to API. Added sample calls to API on client side.

--- client/js/base.js
+++ client/js/base.js
@@ -1,8 +1,37 @@
 $(document).ready(function() {
-  $('form').submit(function(event) {
-    // Stop the browser from submitting the form.
-    event.preventDefault();
+    $('form').submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
 
-    console.log("form was submitted!");
-  });
+        console.log("form was submitted!");
+
+        $.ajax({
+            type: 'POST',
+            url: $(this).attr('action') + '/api/v1/postData',
+            contentType: 'application/json',
+            data: JSON.stringify($(this).serializeJSON()),
+            success: function(data, textStatus, jqXHR) {
+                $('#id_post_result').html(data.message);
+            }
+        })
+    });
+
+    $('#id_get_data').on('click', function(event) {
+        console.log("button was clicked!");
+
+        $.ajax({
+            type: 'GET',
+            url: $('form').attr('action') + '/api/v1/getData',
+            contentType: 'application/json',
+            success: function(data, textStatus, jqXHR) {
+                var resultsTable = $('#id_results');
+                resultsTable.empty();
+                resultsTable.append('<tr><th>Person</th><th>Color</th></tr>');
+                for (var i = 0; i < data.length; i++) {
+                    resultsTable.append('<tr><td>' + data[i].name + '</td>' +
+                        '<td>' + data[i].color + '</td></tr>');
+                }
+            }
+        })
+    });
 });
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var http = require('http');
 var path = require('path');
 
 var express = require('express');
+var bodyParser = require('body-parser');
 
 //
 // ## SimpleServer `SimpleServer(obj)`
@@ -17,9 +18,35 @@ var express = require('express');
 var router = express();
 var server = http.createServer(router);
 
+router.use(bodyParser.json());
+
 router.use(express.static(path.resolve(__dirname, 'client')));
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Server listening at", addr.address + ":" + addr.port);
+// API Routes
+router.get('/api/v1/getData', function(request, response) {
+    var data = [
+        {name: 'Ryan', color: 'red'},
+        {name: 'Trenton', color: 'orange'},
+        {name: 'Rachel', color: 'yellow'},
+        {name: 'Nolan', color: 'green'},
+        {name: 'Trevor', color: 'blue'},
+        {name: 'Nathan', color: 'purple'}
+    ];
+    response.send(data);
+});
+
+router.post('/api/v1/postData', function(request, response) {
+    var color = request.body.color;
+    if (color) {
+        color = color.charAt(0).toUpperCase() + color.substring(1) + ' ';
+    }
+    var data = {
+        message: color + 'Monday??'
+    };
+    response.send(data);
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+    var addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port);
 });
